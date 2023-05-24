427814025ba33210837e70f4e716202778001c2e
techmoves <muhndlovu28@gmail.com>
2023-05-24 17:33:57 +0200

    Add api

---
+++ modules/score.js
@@ -0,0 +1,17 @@
+const scoreData = async () => {
+  const ID = '4iWhaPUyox6pHAr1pbvY';
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores/`;
+  const retrieveScores = async () => {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    return res.json();
+  };
+  const res = await retrieveScores().then((data) => data.result);
+  return res;
+};
+
+export default scoreData;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import './style.css';
 import data from '../modules/app.js';
+import add from '../modules/add.js';
+import scoreData from '../modules/score.js';
 import showScores from '../modules/showscores.js';
+import ClearAllScores from '../modules/clearScore.js';
 
 const form = document.querySelector('.form');
-showScores();
+const refresh = document.querySelector('.Refresh');
 
-form.addEventListener('submit', (e) => {
+refresh.addEventListener('click', () => {
+  ClearAllScores();
+});
+
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const name = form.name.value;
   const score = form.score.value;
   data(name, score);
+  add(form.name.value, form.score.value);
   showScores();
   form.reset();
 });
+showScores();
+scoreData();
