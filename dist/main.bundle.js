378900edb9bcf339bfba3627f15b72f9d5d037f6
techmoves <muhndlovu28@gmail.com>
2023-05-25 12:29:08 +0200

    Fix the refresh btn

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -36,7 +36,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _showscores_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./showscores.js */ \"./modules/showscores.js\");\n\n\nconst ClearAllScores = () => {\n  localStorage.setItem('scores', JSON.stringify([]));\n  (0,_showscores_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ClearAllScores);\n\n//# sourceURL=webpack://leaderboard/./modules/clearScore.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _showscores_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./showscores.js */ \"./modules/showscores.js\");\n\n\nconst ClearAllScores = () => {\n  (0,_showscores_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ClearAllScores);\n\n//# sourceURL=webpack://leaderboard/./modules/clearScore.js?");
 
 /***/ }),
 
--- modules/clearScore.js
+++ modules/clearScore.js
@@ -1,7 +1,6 @@
 import showScores from './showscores.js';
 
 const ClearAllScores = () => {
-  localStorage.setItem('scores', JSON.stringify([]));
   showScores();
 };
 
